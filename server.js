47f41fd08e2366e7c8f3b1d938a103dec272e244
Stephen Chapman <schapm16@gmail.com>
2017-12-13 00:02:21 +0000

    Set up basic file structure.  Set up database schema and seed file.  Begin importing appropriate modules (not complete).  Set up basic express server to work with Heroku.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+//Set up express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//Apply middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+//Server Listen
+app.listen(PORT, function() {
+    console.log("Listening on PORT " + PORT);
+});
