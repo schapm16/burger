a6d315e823050911ce23eadb8c9f40abd1e65891
Stephen Chapman <schapm16@gmail.com>
2017-12-15 03:24:23 +0000

    Add mysql connection for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({ //using mysql default host and port
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({ //using mysql default host and port
     user: 'root',
     password: '',
     database: 'burgers_db'
-});
+  });
+}
 
 connection.connect(function(err) {
-   if (err) throw err;
-   console.log ('connected as id ' + connection.threadId);
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
